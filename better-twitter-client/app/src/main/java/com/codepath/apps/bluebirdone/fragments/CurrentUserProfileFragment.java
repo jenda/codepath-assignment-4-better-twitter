67f837dfd310ed7f9d12011640e1090eb5c3c1bd
Jan Spidlen <jan.spidlen@airbnb.com>
2017-10-07 17:24:14 -0700

    Adding displaying its own tweets.

--- better-twitter-client/app/src/main/java/com/codepath/apps/bluebirdone/fragments/CurrentUserProfileFragment.java
+++ better-twitter-client/app/src/main/java/com/codepath/apps/bluebirdone/fragments/CurrentUserProfileFragment.java
@@ -1,29 +1,27 @@
 package com.codepath.apps.bluebirdone.fragments;
 
 import android.app.Dialog;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.bluebirdone.R;
-import com.codepath.apps.bluebirdone.TwitterClient;
 import com.codepath.apps.bluebirdone.dialogs.BaseBlueBirdOneDialog;
 import com.codepath.apps.bluebirdone.models.CurrentUser;
-import com.codepath.apps.bluebirdone.models.ModelSerializer;
-import com.codepath.apps.bluebirdone.models.User;
-import com.codepath.apps.bluebirdone.twitter.DataConnector;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.codepath.apps.bluebirdone.fragments.TweetsDisplayingFragment.newInstance;
 
 /**
  * Created by jan_spidlen on 10/6/17.
@@ -39,6 +37,9 @@ public class CurrentUserProfileFragment extends BaseBlueBirdOneDialog {
     TextView userFullNameTextView;
     @BindView(R.id.user_handle)
     TextView userHandleTextView;
+
+    @BindView(R.id.tweets_container)
+    FrameLayout frameLayout;
 
     @Inject
     public CurrentUserProfileFragment() {
@@ -61,7 +62,6 @@ public class CurrentUserProfileFragment extends BaseBlueBirdOneDialog {
         userHandleTextView.setText(currentUser.getHandle());
 
         Log.d("jenda", "user id" + currentUser.id);
-
         return view;
     }
     @Override
@@ -75,12 +75,23 @@ public class CurrentUserProfileFragment extends BaseBlueBirdOneDialog {
         }
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
 
+        createAndAttachTweetsFragment();
+
+    }
+
+    private void createAndAttachTweetsFragment() {
+        TweetsDisplayingFragment tweetsDisplayingFragment =
+                TweetsDisplayingFragment.newInstance(
+                        TweetsDisplayingFragment.Type.USER_TWEETS, currentUser.id);
+        this.getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.tweets_container, tweetsDisplayingFragment)
+                .commit();
     }
 }
